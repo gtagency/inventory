a0892ec9c9bf7ebc1ce63adbbf860e1267882e5c
Thomas Shields <thomas.shields@gatech.edu>
2013-07-17 23:14:55 -0500

    basic model structure and admin interface

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+	name = models.CharField(max_length=100)
+	gtID = models.IntegerField(primary_key=True)
+	
+	def __str__(self):
+		return name
+	
+	def __unicode(self):
+		return self.__str__()
+
+class Item(models.Model):
+	title = models.CharField(max_length=200)
+	code = models.IntegerField(primary_key=True)
+	checked_out = models.BooleanField(default=False)
+	
+	#if not checked_out, last person (or None) to check out
+	#if checked_out, current Person 
+	owner = models.ForeignKey(Person)
+
+	def __str__(self):
+		return name
+	
+	def __unicode(self):
+		return self.__str__()
